24b2441c24db57b7bdeacb31ff188e191433831f
HoangTran0308 <tranthanhhoang29@gmail.com>
2024-09-06 19:20:38 +0700

    Hoan thanh BaiTapVeNha02

---
+++ BaiTapVeNha02/Controllers/Tuan02Controller.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BaiTapVeNha02.Controllers
+{
+    public class Tuan02Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            ViewBag.HoTen = "Trần Thanh Hoàng";
+            ViewBag.MSSV = "1822040206";
+            ViewBag.Nam = "Trần Thanh Hoàng - 2024";
+            return View();
+        }
+    }
+}
